99f54f3891a9a10d1d3d060ba5627372c75a4813
KateLencor <107582899+KateLencor@users.noreply.github.com>
2022-07-02 15:14:23 +0300

    Add files via upload
    Задача 1 и 2 из 1 семинара

---
+++ C_1_1.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число  ");
+string Chislo1 = Console.ReadLine();
+double a = double.Parse(Chislo1);
+Console.Write("Введите второе число  ");
+string Chislo2 = Console.ReadLine();
+double b = double.Parse(Chislo2);
+if (a > b)
+{
+    Console.WriteLine("max = "  + a  );
+    Console.Write("min = " + b   );
+}
+else
+{
+    Console.WriteLine("max = " + b   );
+    Console.Write("min = " + a   );
+}
---
+++ C_1_2.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число  ");
+string Chislo1 = Console.ReadLine();
+double a = double.Parse(Chislo1);
+Console.Write("Введите второе число  ");
+string Chislo2 = Console.ReadLine();
+double b = double.Parse(Chislo2);
+Console.Write("Введите третье число  ");
+string Chislo3 = Console.ReadLine();
+double c = double.Parse(Chislo3);
+if (a > b && a > c )
+{
+    Console.WriteLine("max = "  + a  );
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine("max = "  + b  );
+}
+else
+{
+    Console.WriteLine("max = "  + c  );
+}
+
